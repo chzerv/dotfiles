a7cd2709fa3b045826573ecc2b88e052f0f36302
Christos Zervakis <xrist.zerv@protonmail.com>
2020-09-04 22:53:24 +0300

    Emacs: Testing LSP for YAML.
    Seems good so far. You can even read some Ansible docs in
    lsp-ui popups.

--- config/doom/init.el
+++ config/doom/init.el
@@ -163,7 +163,8 @@
        ;;swift             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
        web               ; the tubes
-       yaml              ; JSON, but readable
+       (yaml              ; JSON, but readable
+        +lsp)
 
        :email
        ;; (mu4e +gmail)
