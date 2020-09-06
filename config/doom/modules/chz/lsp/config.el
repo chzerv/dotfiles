de829e110bedd40bf8b3c9bc440e2bc8d1e1cb63
Chr <xrist.zerv@protonmail.com>
2020-09-06 09:27:56 +0300

    Emacs: Add a delay to lsp-ui-doc.
    Having a posframe popup immediately after typing is annoying.

--- config/doom/modules/chz/lsp/config.el
+++ config/doom/modules/chz/lsp/config.el
@@ -1,17 +1,18 @@
 ;;; chz/lsp/config.el -*- lexical-binding: t; -*-
 
+;; Performance tweaks
 (setq read-process-output-max (* 2 1024 1024)) ;; 2mb
-
 (after! lsp
-  (setq lsp-completion-provider :capf))
+  (setq lsp-completion-provider :capf
+        lsp-idle-delay 0.500))
 
 (after! lsp-ui
   (setq lsp-ui-sideline-enable nil
         lsp-ui-doc-enable t
         lsp-ui-doc-header t
         lsp-ui-doc-position 'at-point ;; top, bottom, or at-point
+        lsp-ui-doc-delay 0.500
         lsp-ui-peek-enable t
         lsp-ui-peek-fontify 'on-demand)
 
   (map! :ne "C-S-k" 'lsp-ui-doc-glance))
-
