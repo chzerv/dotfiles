df171e379f97fc4a0a380d8e47b02c012832c707
Christos Zervakis <xrist.zerv@protonmail.com>
2020-11-30 10:31:16 +0200

    Emacs: Forked ivy-posframe is no longer needed.

--- config/doom/modules/private/ivy/packages.el
+++ config/doom/modules/private/ivy/packages.el
@@ -3,6 +3,4 @@
 
 ;; Doom also installs ivy-posframe when the '+childframe' option in enabled
 ;; for ivy, but I don't like the defaults.
-(package! ivy-posframe
-  :recipe (:host github
-           :repo "snyball/ivy-posframe"))
+(package! ivy-posframe :pin "58ddf8c1622dfa2c91b6bfe6fee842abeff0b422")
