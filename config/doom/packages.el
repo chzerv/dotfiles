4ee91f7b41c9fd83cf6709904019f39fa8ca68fd
Christos Zervakis <xrist.zerv@protonmail.com>
2020-12-20 10:48:25 +0200

    Emacs: Upload the updated packages.el.

--- config/doom/packages.el
+++ config/doom/packages.el
@@ -1,17 +1,9 @@
 ;; -*- no-byte-compile: t; -*-
-;;; $DOOMDIR/packages.el
 
-;; Themes
-(package! modus-operandi-theme)
-(package! modus-vivendi-theme)
-(package! color-theme-sanityinc-tomorrow)
-
-;; Unpin packages
 (unpin! doom-modeline)
 (unpin! doom-themes)
 (unpin! vterm)
 
-;; Disable packages
 (package! evil-escape :disable t)
 (package! evil-quick-diff :disable t)
 (package! evil-traces :disable t)
@@ -24,3 +16,51 @@
 (package! glsl-mode :disable t)
 (package! company-glsl :disable t)
 (package! flyspell-lazy :disable t)
+
+(package! modus-operandi-theme)
+(package! modus-vivendi-theme)
+(package! color-theme-sanityinc-tomorrow)
+
+(package! imenu-list)
+
+(package! dired-open)
+
+(package! dired-subtree)
+
+(package! dired-narrow)
+
+(package! peep-dired)
+
+(package! dired-du)
+
+(package! ace-window)
+
+(package! highlight-indent-guides :pin "cf352c85cd15dd18aa096ba9d9ab9b7ab493e8f6")
+
+(package! org-mode
+  :recipe (:host github
+           :repo "yantar92/org"
+           :branch "feature/org-fold"
+           :files ("*.el" "lisp/*.el" "contrib/lisp/*.el")
+           :build (with-temp-file (expand-file-name "org-version.el" (straight--repos-dir "org"))
+                    (insert "(fset 'org-release (lambda () \"9.5\"))\n"
+                            "(fset 'org-git-version #'ignore)\n"
+                            "(provide 'org-version)\n")))
+  :pin "b0ad4095f1d65224642439822d30b88f566efb92"
+  :shadow 'org)
+
+(package! org-protocol-capture-html
+  :recipe (:host github :repo "alphapapa/org-protocol-capture-html")
+  :pin "3359ce9a2f3b48df26329adaee0c4710b1024250")
+
+(package! org-wild-notifier)
+
+;; (package! org-roam-server)
+
+(package! org-super-agenda)
+
+(package! olivetti)
+
+(package! pkgbuild-mode)
+
+(package! systemd)
