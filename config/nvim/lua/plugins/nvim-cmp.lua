309ebc9165774994f4fabf96b319ebee39a1e09e
Chris Zervakis <xrist.zerv@protonmail.com>
2022-05-08 16:19:01 +0300

    nvim: Update nvim-cmp config

--- config/nvim/lua/plugins/nvim-cmp.lua
+++ config/nvim/lua/plugins/nvim-cmp.lua
@@ -1,81 +1,89 @@
---------------------------------
+-----------------------------------------
 -- Configuration for nvim-cmp
---------------------------------
+-- https://github.com/hrsh7th/nvim-cmp
+-----------------------------------------
 
-local cmp = require('cmp')
-local snippy = require('snippy')
+local cmp_ok, cmp = pcall(require, 'cmp')
+if not cmp_ok then
+    return
+end
+
+local snippy_ok, snippy = pcall(require, 'snippy')
+if not snippy_ok then
+    return
+end
 
 cmp.setup({
-  -- Specify a snippet engine
-  snippet = {
-    expand = function(args)
-        snippy.expand_snippet(args.body)
-    end,
-  },
+    -- Specify a snippet engine
+    snippet = {
+        expand = function(args)
+            snippy.expand_snippet(args.body)
+        end,
+    },
 
-  -- Completion options
-  completion = {
+    -- Completion options
+    completion = {
         completeopt = "menu,menuone,noinsert",
         keyword_pattern = [[\%(-\?\d\+\%(\.\d\+\)\?\|\h\w*\%(-\w*\)*\)]],
         keyword_length = 1,
     },
 
-  -- Key mapping
-  mapping = cmp.mapping.preset.insert {
-    ['<C-n>'] = cmp.mapping.select_next_item(),
-    ['<C-p>'] = cmp.mapping.select_prev_item(),
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.close(),
-    ['<CR>'] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
+    -- Key mapping
+    mapping = cmp.mapping.preset.insert {
+        ['<C-n>'] = cmp.mapping.select_next_item(),
+        ['<C-p>'] = cmp.mapping.select_prev_item(),
+        ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<C-e>'] = cmp.mapping.close(),
+        ['<CR>'] = cmp.mapping.confirm {
+            behavior = cmp.ConfirmBehavior.Replace,
+            select = true,
+        },
+
+        -- Tab mapping
+        ['<Tab>'] = function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif snippy.can_expand_or_advance() then
+                snippy.expand_or_advance()
+            else
+                fallback()
+            end
+        end,
+        ['<S-Tab>'] = function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif snippy.can_jump(-1) then
+                snippy.snippy.previous()
+            else
+                fallback()
+            end
+        end
     },
-
-    -- Tab mapping
-    ['<Tab>'] = function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif snippy.can_expand_or_advance() then
-        snippy.expand_or_advance()
-      else
-        fallback()
-      end
-    end,
-    ['<S-Tab>'] = function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif snippy.can_jump(-1) then
-          snippy.snippy.previous()
-      else
-        fallback()
-      end
-    end
-  },
-
-  -- Load sources, see: https://github.com/topics/nvim-cmp
-  sources = {
-    { name = 'nvim_lsp' },
-    { name = 'snippy' },
-    { name = 'path' },
-    { name = 'buffer' },
-    { name = 'cmdline' },
-  },
-})
-
-cmp.setup.cmdline('/', {
-    mapping = cmp.mapping.preset.cmdline(),
     sources = {
-        { name = 'buffer' },
-        { name = 'cmdline' }
+        { name = 'nvim_lsp' },
+        { name = 'nvim_lsp_signature_help' },
+        { name = 'snippy' },
+        { name = 'path' },
+        { name = 'buffer' }
     }
 })
 
--- cmp.setup.cmdline(':', {
---     mapping = cmp.mapping.preset.cmdline(),
---     sources = {
---         { name = 'path' },
---         { name = 'cmdline' }
---     }
--- })
+-- Use buffer source for `/`
+cmp.setup.cmdline('/', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+        { name = 'buffer' }
+    }
+})
+
+-- Use cmdline & path source for ':'
+cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+        { name = 'path' }
+    }, {
+        { name = 'cmdline' }
+    })
+})
