1f6177d4086b82a88343b3e7495ec5740db0d89c
Chris Zervakis <xrist.zerv@protonmail.com>
2022-05-15 13:04:09 +0300

    nvim: Update treesitter config

--- config/nvim/lua/plugins/nvim-treesitter.lua
+++ config/nvim/lua/plugins/nvim-treesitter.lua
@@ -8,15 +8,42 @@ if not ok then
     return
 end
 
-ts.setup({
-    matchup = {
-        enable = true,
-    },
+require"nvim-treesitter.configs".setup({
     ensure_installed = {
         'bash', 'c', 'dockerfile', 'go', 'javascript', 'python', 'json', 'rust', 'yaml', 'vim', 'lua', 'html'
     },
     sync_install = false,
     highlight = {
-        enable = true
-    }
+        enable = true,
+        disable = {
+            "help",
+        },
+    },
+    indent = {
+        enable = true,
+    },
+    textobjects = {
+        move = {
+            enable = true,
+            set_jumps = true, -- Set jumps in the jumplist
+            goto_next_start = {
+                ["]m"] = "@function.outer",
+            },
+            goto_next_end = {
+                ["]M"] = "@function.outer",
+            },
+            goto_previous_start = {
+                ["[m"] = "@function.outer",
+            },
+            goto_previous_end = {
+                ["[M"] = "@function.outer"
+            },
+        },
+        select = {
+            enable = true,
+            lookahead = true,
+        },
+    },
 })
+
+
