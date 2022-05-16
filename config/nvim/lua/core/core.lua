d5879867997cd203f09c09ba7a46b216493df414
Chris Zervakis <xrist.zerv@protonmail.com>
2022-05-17 02:55:23 +0300

    nvim: Disable builtin plugins

--- config/nvim/lua/core/core.lua
+++ config/nvim/lua/core/core.lua
@@ -68,3 +68,28 @@ end
 opt.joinspaces = false -- Don't join lines with spaces at the end
 opt.breakindent = true -- Indent wrapped lines
 -- }}}
+
+-- Disable builtin plugins
+
+g.loaded_gzip = 1
+g.loaded_zip = 1
+g.loaded_zipPlugin = 1
+g.loaded_tar = 1
+g.loaded_tarPlugin = 1
+g.loaded_getscript = 1
+g.loaded_getscriptPlugin = 1
+g.loaded_vimball = 1
+g.loaded_vimballPlugin = 1
+g.loaded_2html_plugin = 1
+g.loaded_matchit = 1
+g.loaded_matchparen = 1
+g.loaded_logiPat = 1
+g.loaded_rrhelper = 1
+g.loaded_netrw = 1
+g.loaded_netrwPlugin = 1
+g.loaded_netrwSettings = 1
+g.loaded_man = 1
+g.loaded_remote_plugins = 1
+g.loaded_shada_plugin = 1
+g.loaded_spellfile_plugin = 1
+g.loaded_tutor_mode_plugin= 1
