e2fa7f0c914c52f031ec5230c0fa97702f01b269
Chris Zervakis <xrist.zerv@protonmail.com>
2022-05-24 01:25:35 +0300

    nvim: gitsigns tweaks

--- config/nvim/lua/plugins/gitsigns.lua
+++ config/nvim/lua/plugins/gitsigns.lua
@@ -9,14 +9,21 @@ if not status_ok then
 end
 
 gitsigns.setup {
-    trouble = true,
+    -- trouble = true,
+    signs = {
+    add          = {hl = 'GitSignsAdd'   , text = '│', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'},
+    change       = {hl = 'GitSignsChange', text = '│', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+    delete       = {hl = 'GitSignsDelete', text = '_', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
+    topdelete    = {hl = 'GitSignsDelete', text = '‾', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
+    changedelete = {hl = 'GitSignsChange', text = '~', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
+  },
 }
 
 -- Cycle through git hunks
 local opts = { noremap = true, silent = true }
 local map = vim.api.nvim_set_keymap
-map('n', '<Leader>g]', [[<Cmd>Gitsigns next_hunk<CR>]], opts) -- Move to the next hunk
-map('n', '<Leader>g[', [[<Cmd>Gitsigns prev_hunk<CR>]], opts) -- Move to the previous hunk
+map('n', ']g', [[<Cmd>Gitsigns next_hunk<CR>]], opts) -- Move to the next hunk
+map('n', '[g', [[<Cmd>Gitsigns prev_hunk<CR>]], opts) -- Move to the previous hunk
 map('n', '<Leader>gs', [[<Cmd>Gitsigns stage_hunk<CR>]], opts) -- Stage hunk under cursor
 map('n', '<Leader>gb', [[<Cmd>Gitsigns stage_buffer<CR>]], opts) -- Stage whole buffer
 map('n', '<Leader>gp', [[<Cmd>Gitsigns preview_hunk<CR>]], opts) -- Move to the next hunk
