bf6d3127685222d0cedb5451276de1e5bcba1307
Chris Zervakis <xrist.zerv@protonmail.com>
2022-05-24 14:39:30 +0300

    nvim: Prettier diagnostics

--- config/nvim/lua/plugins/lsp/diagnostics.lua
+++ config/nvim/lua/plugins/lsp/diagnostics.lua
@@ -1,12 +1,23 @@
 local M = {}
 
+vim.fn.sign_define("DiagnosticSignError", { text = "", texthl = "DiagnosticSignError" })
+vim.fn.sign_define("DiagnosticSignWarn", { text = "", texthl = "DiagnosticSignWarn" })
+vim.fn.sign_define("DiagnosticSignInfo", { text = "", texthl = "DiagnosticSignInfo" })
+vim.fn.sign_define("DiagnosticSignHint", { text = "", texthl = "DiagnosticSignHint" })
+
+
 M.setup = function()
     vim.diagnostic.config({
+        -- Don't show diagnostics at the end of the line
         virtual_text = false,
         signs = true,
         underline = true,
         update_in_insert = false,
-        severity_sort = false,
+        severity_sort = true,
+        float = {
+            source = "always",
+            border = "rounded",
+        }
     })
 end
 
