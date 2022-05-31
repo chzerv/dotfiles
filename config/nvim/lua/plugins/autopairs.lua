270d3cb6187bcd6c8225d35b6482064660bf0e34
Chris Zervakis <xrist.zerv@protonmail.com>
2022-05-31 14:31:33 +0300

    nvim: Add spaces after inserting an auto-pair

--- config/nvim/lua/plugins/autopairs.lua
+++ config/nvim/lua/plugins/autopairs.lua
@@ -8,7 +8,38 @@ if not ok then
     return
 end
 
+local Rule = require'nvim-autopairs.rule'
+local cond = require'nvim-autopairs.conds'
+
 autopairs.setup({
     enable_check_bracket_line = true,
     disable_filetype = { 'TelescopePrompt' },
 })
+
+-- Add space between (), {}, and []
+-- https://github.com/windwp/nvim-autopairs/wiki/Custom-rules#add-spaces-between-parentheses=
+autopairs.add_rules {
+  Rule(' ', ' ')
+    :with_pair(function (opts)
+      local pair = opts.line:sub(opts.col - 1, opts.col)
+      return vim.tbl_contains({ '()', '[]', '{}' }, pair)
+    end),
+  Rule('( ', ' )')
+      :with_pair(function() return false end)
+      :with_move(function(opts)
+          return opts.prev_char:match('.%)') ~= nil
+      end)
+      :use_key(')'),
+  Rule('{ ', ' }')
+      :with_pair(function() return false end)
+      :with_move(function(opts)
+          return opts.prev_char:match('.%}') ~= nil
+      end)
+      :use_key('}'),
+  Rule('[ ', ' ]')
+      :with_pair(function() return false end)
+      :with_move(function(opts)
+          return opts.prev_char:match('.%]') ~= nil
+      end)
+      :use_key(']')
+}
