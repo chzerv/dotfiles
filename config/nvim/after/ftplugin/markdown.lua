c39a3aa34ef0d109dc0389040236cc12cea34d8e
Chris Zervakis <xrist.zerv@protonmail.com>
2022-06-12 22:47:28 +0300

    nvim: Disable list & wrapping in markdown files

--- config/nvim/after/ftplugin/markdown.lua
+++ config/nvim/after/ftplugin/markdown.lua
@@ -2,3 +2,6 @@ vim.opt_local.spell = true
 vim.opt_local.spelllang = "en_us"
 vim.opt_local.foldmethod = "manual"
 vim.opt_local.conceallevel = 2
+vim.opt_local.list = false
+vim.opt_local.wrap = false
+vim.opt_local.formatoptions:append("t")
