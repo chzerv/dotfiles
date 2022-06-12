76fa687b16a4beb277187cbc91eebce17bff25da
Chris Zervakis <xrist.zerv@protonmail.com>
2022-06-12 23:38:30 +0300

    nvim: Fterm tweaks

--- config/nvim/lua/plugins/fterm.lua
+++ config/nvim/lua/plugins/fterm.lua
@@ -21,21 +21,34 @@ local lazygit = fterm:new({
     },
 })
 
-function _G.__fterm_lazygit()
-    lazygit:toggle()
-end
-
 local python = fterm:new({
     ft = "fterm_python",
-    cmd = "python",
+    cmd = "bpython",
     dimensions = {
         height = 0.8,
         width = 0.8,
     },
 })
 
+local ranger = fterm:new({
+    ft = "fterm_ranger",
+    cmd = "ranger",
+    dimensions = {
+        height = 0.9,
+        width = 0.9,
+    },
+})
+
+function _G.__fterm_lazygit()
+    lazygit:toggle()
+end
+
 function _G.__fterm_python()
     python:toggle()
+end
+
+function _G.__fterm_ranger()
+    ranger:toggle()
 end
 
 -- Example keybindings
@@ -43,6 +56,7 @@ local opts = { noremap = true }
 vim.keymap.set("n", "<A-i>", '<CMD>lua require("FTerm").toggle()<CR>', opts)
 vim.keymap.set("t", "<A-i>", '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>', opts)
 vim.keymap.set("n", "<A-g>", "<CMD>lua __fterm_lazygit()<CR>", opts)
+vim.keymap.set("n", "<A-r>", "<CMD>lua __fterm_ranger()<CR>", opts)
 
 vim.api.nvim_create_user_command("CargoRun", function()
     require("FTerm").scratch({ cmd = { "cargo", "run" } })
@@ -53,4 +67,5 @@ vim.api.nvim_create_user_command("AnsiblePlaybook", function()
 end, { bang = true })
 
 vim.api.nvim_create_user_command("LAZYGIT", "lua __fterm_lazygit()", { bang = true })
+vim.api.nvim_create_user_command("Ranger", "lua __fterm_ranger()", { bang = true })
 vim.api.nvim_create_user_command("PYTHON", "lua __fterm_python()", { bang = true })
