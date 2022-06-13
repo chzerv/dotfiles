075cbbed7cefe62cbac9c4054725fff8fd6c671c
Chris Zervakis <xrist.zerv@protonmail.com>
2022-06-13 21:49:40 +0300

    stuff

--- scripts/charging.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-/usr/bin/powerprofilesctl set balanced && \
-    echo 1 > /sys/devices/system/cpu/cpufreq/boost
--- scripts/discharging.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-/usr/bin/powerprofilesctl set power-saver && \
-    echo 0 > /sys/devices/system/cpu/cpufreq/boost
