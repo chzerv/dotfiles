99b0c8986d6ffdfec80c5834601e47c4be91cbf9
Chris Zervakis <xrist.zerv@protonmail.com>
2022-06-13 21:50:17 +0300

    nvim: Lua snippet for packer use

--- config/nvim/lua/plugins/snippets/lua.lua
+++ config/nvim/lua/plugins/snippets/lua.lua
@@ -21,52 +21,78 @@ local conds = require("luasnip.extras.expand_conditions")
 ls.add_snippets("lua", {
     s(
         "req",
-        fmt([[
+        fmt(
+            [[
             local {} = require "{}"
-        ]], {
-            f(function(args)
-                local pairs = vim.split(args[1][1], ".", true)
-                return pairs[#pairs] or ""
-            end, { 1 }),
-            i(1),
-        })
+        ]],
+            {
+                f(function(args)
+                    local pairs = vim.split(args[1][1], ".", true)
+                    return pairs[#pairs] or ""
+                end, { 1 }),
+                i(1),
+            }
+        )
     ),
     s(
         "if",
-        fmt([[
+        fmt(
+            [[
         if {} then
             {}
         end
         {}
-        ]], { i(1, "condition"), i(2, "body"), i(3) })
+        ]],
+            { i(1, "condition"), i(2, "body"), i(3) }
+        )
     ),
     s(
         "pcall",
-        fmt([[
+        fmt(
+            [[
         local ok, {} = pcall(require, "{}")
-        ]], {
+        ]],
+            {
                 f(function(args)
                     local pairs = vim.split(args[1][1], ".", true)
                     return pairs[#pairs] or ""
-                end, { 1}),
+                end, { 1 }),
                 i(1),
-            })
+            }
+        )
     ),
     s(
         "aucmd",
-        fmt([[
+        fmt(
+            [[
         local group = vim.api.nvim_create_augroup("{}", {{ clear = true }})
         vim.api.nvim_create_autocmd("{}", {{
            group = group,
            {} = {}
         }})
         {}
-        ]], {
-            i(1, "group name"),
-            i(2, "event"),
-            c(3, { t"command", t"callback"} ),
-            i(4, "body"),
-            i(5)
-            })
+        ]],
+            {
+                i(1, "group name"),
+                i(2, "event"),
+                c(3, { t("command"), t("callback") }),
+                i(4, "body"),
+                i(5),
+            }
+        )
+    ),
+    s(
+        "packuse",
+        fmt(
+            [[
+        use({{
+            "{}",
+            config = function()
+                require("plugins.{}")
+            end
+        }})
+        ]],
+            { i(1, "plugin"), i(2, "config") }
+        )
     ),
 })
