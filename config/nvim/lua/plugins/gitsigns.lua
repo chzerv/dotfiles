e8537c2e2e2ccdbfc1f4ada5c5e60a7937b81218
Chris Zervakis <xrist.zerv@protonmail.com>
2022-06-25 15:45:12 +0300

    nvim: Update key bindings for gitsigns

--- config/nvim/lua/plugins/gitsigns.lua
+++ config/nvim/lua/plugins/gitsigns.lua
@@ -5,27 +5,26 @@
 
 local status_ok, gitsigns = pcall(require, "gitsigns")
 if not status_ok then
-  return
+    return
 end
 
-gitsigns.setup {
+gitsigns.setup({
     -- trouble = true,
     signs = {
-    add          = {hl = 'GitSignsAdd'   , text = '│', numhl='GitSignsAddNr'   , linehl='GitSignsAddLn'},
-    change       = {hl = 'GitSignsChange', text = '│', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
-    delete       = {hl = 'GitSignsDelete', text = '_', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-    topdelete    = {hl = 'GitSignsDelete', text = '‾', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
-    changedelete = {hl = 'GitSignsChange', text = '~', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
-  },
-}
+        add = { hl = "GitSignsAdd", text = "│", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
+        change = { hl = "GitSignsChange", text = "│", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+        delete = { hl = "GitSignsDelete", text = "_", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+        topdelete = { hl = "GitSignsDelete", text = "‾", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+        changedelete = { hl = "GitSignsChange", text = "~", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+    },
+})
 
 -- Cycle through git hunks
 local opts = { noremap = true, silent = true }
-local map = vim.api.nvim_set_keymap
-map('n', ']g', [[<Cmd>Gitsigns next_hunk<CR>]], opts) -- Move to the next hunk
-map('n', '[g', [[<Cmd>Gitsigns prev_hunk<CR>]], opts) -- Move to the previous hunk
-map('n', '<Leader>gs', [[<Cmd>Gitsigns stage_hunk<CR>]], opts) -- Stage hunk under cursor
-map('n', '<Leader>gb', [[<Cmd>Gitsigns stage_buffer<CR>]], opts) -- Stage whole buffer
-map('n', '<Leader>gp', [[<Cmd>Gitsigns preview_hunk<CR>]], opts) -- Preview hunk
-map('n', '<Leader>gd', [[<Cmd>Gitsigns reset_hunk<CR>]], opts) -- Undo hunk
-
+local map = vim.keymap.set
+map("n", "]g", [[<Cmd>Gitsigns next_hunk<CR>]], opts) -- Move to the next hunk
+map("n", "[g", [[<Cmd>Gitsigns prev_hunk<CR>]], opts) -- Move to the previous hunk
+map({ "n", "x" }, "<Leader>gs", [[<Cmd>Gitsigns stage_hunk<CR>]], opts) -- Stage hunk under cursor
+map("n", "<Leader>gb", [[<Cmd>Gitsigns stage_buffer<CR>]], opts) -- Stage whole buffer
+map("n", "<Leader>gp", [[<Cmd>Gitsigns preview_hunk<CR>]], opts) -- Preview hunk
+map({ "n", "x" }, "<Leader>gd", [[<Cmd>Gitsigns reset_hunk<CR>]], opts) -- Undo hunk
