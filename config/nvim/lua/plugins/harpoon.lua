f72accec348b37239f912f1ea2cbe7c36d2d9332
Chris Zervakis <xrist.zerv@protonmail.com>
2022-07-15 17:25:11 +0300

    nvim: Ditch nmap & imap utility functions

--- config/nvim/lua/plugins/harpoon.lua
+++ config/nvim/lua/plugins/harpoon.lua
@@ -1,30 +1,18 @@
-local nmap = require("core.utils").nmap
+local map = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
-nmap({
-    "<leader>ha",
-    function()
-        require("harpoon.mark").add_file()
-    end,
-   opts,
-})
+map("n", "<leader>ha", function()
+    require("harpoon.mark").add_file()
+end, opts)
 
-nmap({
-    "<leader>hh",
-    function()
-        require("harpoon.ui").toggle_quick_menu()
-    end,
-    opts,
-})
+map("n", "<leader>hh", function()
+    require("harpoon.ui").toggle_quick_menu()
+end, opts)
 
 for i = 1, 5 do
-    nmap({
-        string.format("<leader>%s", i),
-        function()
-            require("harpoon.ui").nav_file(i)
-        end,
-        opts,
-    })
+    map("n", string.format("<leader>%s", i), function()
+        require("harpoon.ui").nav_file(i)
+    end, opts)
 end
 
 -- Open harpoon target in a vertical split.
