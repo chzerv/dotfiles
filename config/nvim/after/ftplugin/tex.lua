8438481f097f778584e0f8271ab37b827f096adb
Chris Zervakis <xrist.zerv@protonmail.com>
2022-07-22 11:33:12 +0300

    feat(nvim): Add environment surround for LaTeX

--- config/nvim/after/ftplugin/tex.lua
+++ config/nvim/after/ftplugin/tex.lua
@@ -2,3 +2,24 @@ vim.opt_local.spell = true
 vim.opt_local.spelllang = "en_us,el"
 vim.opt_local.shiftwidth = 2
 vim.opt_local.softtabstop = 2
+
+local ok, surround = pcall(require, "nvim-surround")
+if ok then
+
+    -- Use via ySe or VSe
+    surround.buffer_setup {
+        delimiters = {
+            pairs = {
+                ["e"] = function()
+                    local env = require("plugins.nvim-surround").get_input("Enter environment name: ")
+                    if env then
+                        return {
+                            "\\begin{" .. env .. "}",
+                            "\\end{" .. env .. "}"
+                        }
+                    end
+                end
+            },
+        },
+    }
+end
--- config/nvim/lua/plugins/nvim-surround.lua
+++ config/nvim/lua/plugins/nvim-surround.lua
@@ -4,3 +4,15 @@ if not ok then
 end
 
 surround.setup{}
+
+local M = {}
+
+M.get_input = function(prompt)
+    local ok, input = pcall(vim.fn.input, { prompt = prompt })
+    if not ok then
+        return nil
+    end
+    return input
+end
+
+return M
