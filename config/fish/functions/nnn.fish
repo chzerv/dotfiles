ca0881d51eb4ad14aaad164510fc6baac5ba72c5
Chris Zervakis <xrist.zerv@protonmail.com>
2022-08-10 15:08:53 +0300

    feat(fish): nnn wrapper to cd on quit

---
+++ config/fish/functions/nnn.fish
@@ -0,0 +1,32 @@
+# https://github.com/jarun/nnn/wiki/Basic-use-cases#configure-cd-on-quit
+
+function nnn --wraps nnn --description 'support nnn quit and change directory'
+    # Block nesting of nnn in subshells
+    if test -n "$NNNLVL"
+        if [ (expr $NNNLVL + 0) -ge 1 ]
+            echo "nnn is already running"
+            return
+        end
+    end
+
+    if test -n "$XDG_CONFIG_HOME"
+        set -x NNN_TMPFILE "$XDG_CONFIG_HOME/nnn/.lastd"
+    else
+        set -x NNN_TMPFILE "$HOME/.config/nnn/.lastd"
+    end
+
+    # Unmask ^Q (, ^V etc.) (if required, see `stty -a`) to Quit nnn
+    # stty start undef
+    # stty stop undef
+    # stty lwrap undef
+    # stty lnext undef
+
+    # The command function allows one to alias this function to `nnn` without
+    # making an infinitely recursive alias
+    command nnn $argv
+
+    if test -e $NNN_TMPFILE
+        source $NNN_TMPFILE
+        rm $NNN_TMPFILE
+    end
+end
