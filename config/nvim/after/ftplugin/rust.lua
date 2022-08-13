0a120cfcd7cbf5cff53fc15ac0e207432eb5b056
Chris Zervakis <xrist.zerv@protonmail.com>
2022-08-13 10:39:14 +0300

    refactor(nvim): Rust stuff

--- config/nvim/after/ftplugin/rust.lua
+++ config/nvim/after/ftplugin/rust.lua
@@ -1,12 +1,11 @@
-local opts = { noremap = true, silent = true }
-local map = vim.keymap.set
+local has_rust_tools, _ = pcall(require, "rust-tools")
+if has_rust_tools then
 
-map("n", "<leader>rd", "<cmd>RustOpenExternalDocs<cr>", opts)
+    local opts = { noremap = true, silent = true }
+    local map = vim.keymap.set
 
-map("n", "<leader>rr", "<cmd>RustRunnables<cr>", opts)
-
-map("n", "<leader>rc", "<cmd>RustOpenCargo<cr>", opts)
-
-map("n", "<leader>rR", "<cmd>RustOpenCargo<cr>", opts)
-
-map("n", "<leader>rR", "<cmd>CargoReload<cr>", opts)
+    map("n", "<leader>rd", "<cmd>RustOpenExternalDocs<cr>", opts)
+    map("n", "<leader>rr", "<cmd>RustRunnables<cr>", opts)
+    map("n", "<leader>rc", "<cmd>RustOpenCargo<cr>", opts)
+    map("n", "<leader>rR", "<cmd>CargoReload<cr>", opts)
+end
