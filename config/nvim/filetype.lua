a6c697946a015a8338a3ba6a05405adc326a8369
Chris Zervakis <xrist.zerv@protonmail.com>
2022-08-24 10:41:06 +0300

    fix(nvim): Ansible filetype detection

--- config/nvim/filetype.lua
+++ config/nvim/filetype.lua
@@ -3,11 +3,11 @@ vim.filetype.add({
         conf = "conf",
     },
     pattern = {
-        ["main.y(a*)ml"] = "yaml.ansible",
         ["site.y(a*)ml"] = "yaml.ansible",
         ["playbook.y(a*)ml"] = "yaml.ansible",
         [".*/tasks/.*%.y(a*)ml"] = "yaml.ansible",
         [".*/role(s*)/.*%.y(a*)ml"] = "yaml.ansible",
+        [".*/playbook(s*)/.*%.y(a*)ml"] = "yaml.ansible",
         [".*/vars/.*%.y(a*)ml"] = "yaml.ansible",
         [".*/group_vars/.*%.y(a*)ml"] = "yaml.ansible",
         [".*/default(s*)/.*%.y(a*)ml"] = "yaml.ansible",
