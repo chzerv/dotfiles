179cbccecc2c5e1c36ee89a96cac67546a60d02f
Chris Zervakis <xrist.zerv@protonmail.com>
2022-10-26 02:02:11 +0300

    feat(nvim): Add mapping for 'RustDebuggables'

--- config/nvim/after/ftplugin/rust.lua
+++ config/nvim/after/ftplugin/rust.lua
@@ -6,6 +6,7 @@ if has_rust_tools then
 
     map("n", "<leader>rd", "<cmd>RustOpenExternalDocs<cr>", opts)
     map("n", "<leader>rr", "<cmd>RustRunnables<cr>", opts)
+    map("n", "<leader>rd", "<cmd>RustDebuggables<cr>", opts)
     map("n", "<leader>rc", "<cmd>RustOpenCargo<cr>", opts)
     map("n", "<leader>rR", "<cmd>CargoReload<cr>", opts)
 end
--- config/nvim/lua/plugins/lsp/init.lua
+++ config/nvim/lua/plugins/lsp/init.lua
@@ -47,7 +47,7 @@ vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
     { border = "rounded", silent = true, max_height = 20, relative = "cursor" }
 )
 
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 for _, server in ipairs(servers) do
         local opts = {
