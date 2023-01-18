a40f93a75d83128ae6693d07325f8d9f39c9508c
Chris Zervakis <xrist.zerv@protonmail.com>
2023-01-19 00:49:57 +0200

    feat(fish): Bind 'C-z' to 'fg'

--- config/fish/functions/fish_user_key_bindings.fish
+++ config/fish/functions/fish_user_key_bindings.fish
@@ -1,4 +1,6 @@
 function fish_user_key_bindings
   bind ! bind_bang
   bind '$' bind_dollar
+  # https://github.com/fish-shell/fish-shell/issues/7152#issuecomment-663575001
+  bind \cz 'fg 2>/dev/null; commandline -f repaint'
 end
