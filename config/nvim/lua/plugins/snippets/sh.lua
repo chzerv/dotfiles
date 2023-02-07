aa627bc83793319cc3579b7fcdb8a56c7aa5996b
Chris Zervakis <xrist.zerv@protonmail.com>
2023-02-08 00:39:52 +0200

    feat(nvim): Bash snippet to check if a program is installed

--- config/nvim/lua/plugins/snippets/sh.lua
+++ config/nvim/lua/plugins/snippets/sh.lua
@@ -1,31 +1,26 @@
 local ls = require("luasnip")
 local s = ls.snippet
-local sn = ls.snippet_node
 local t = ls.text_node
 local i = ls.insert_node
-local f = ls.function_node
-local c = ls.choice_node local d = ls.dynamic_node
-local r = ls.restore_node
-local l = require("luasnip.extras").lambda
 local rep = require("luasnip.extras").rep
-local p = require("luasnip.extras").partial
-local m = require("luasnip.extras").match
-local n = require("luasnip.extras").nonempty
-local dl = require("luasnip.extras").dynamic_lambda
 local fmt = require("luasnip.extras.fmt").fmt
-local fmta = require("luasnip.extras.fmt").fmta
-local types = require("luasnip.util.types")
-local conds = require("luasnip.extras.expand_conditions")
 
 -- Simple placeholders
 ls.add_snippets("sh", {
-    s(
-        "#!", t"#!/usr/bin/env bash"
-    ),
-    s(
-        "shebang", t"#!/usr/bin/env bash"
-    ),
+    s("#!", t("#!/usr/bin/env bash")),
+    s("shebang", t("#!/usr/bin/env bash")),
     ls.parser.parse_snippet("if", "if [[ ${1:cond} ]]; then\n\t${0:body}\nfi"),
     ls.parser.parse_snippet("elif", "elif [[ ${1:cond} ]]; then\n\t${0:body}"),
+    s(
+        "command_exists",
+        fmt(
+            [[
+        if ! command -v {} &>/dev/null; then 
+            echo "{} not installed." 
+            exit 1
+        fi
+        ]],
+            { i(1, "cmd"), rep(1) }
+        )
+    ),
 })
-
