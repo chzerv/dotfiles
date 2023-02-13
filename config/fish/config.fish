43b0552df138d7a70a0cd09b79128e1c8858f3c3
Chris Zervakis <xrist.zerv@protonmail.com>
2023-02-13 22:32:46 +0200

    feat(fish): Set ANSIBLE_FORCE_COLOR envvar
    Setting this variable is required if we want to have colors in
    molecule's output

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -17,6 +17,7 @@ set -x STARSHIP_CONFIG "$HOME/.config/starship.toml"
 set -x npm_config_prefix "$HOME/.node_modules/"
 set -x RIPGREP_CONFIG_PATH "$HOME/.config/ripgrep.conf"
 set -x SSH_AUTH_SOCK "$XDG_RUNTIME_DIR/ssh-agent.socket"
+set -x ANSIBLE_FORCE_COLOR 1
 
 if status is-interactive
 
