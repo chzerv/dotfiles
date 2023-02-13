7f74796b2ed4b0d434729e8a5c23def0bb273b6e
Chris Zervakis <xrist.zerv@protonmail.com>
2023-02-13 20:23:10 +0200

    fix(nvim): Treat files in .github/workflows/ as YAML

--- config/nvim/filetype.lua
+++ config/nvim/filetype.lua
@@ -5,11 +5,12 @@ vim.filetype.add({
         tfvars = "terraform",
     },
     pattern = {
+        [".*/.github/workflows/.*%.y(a*)ml"] = { "yaml", { priority = 10 } },
         ["site.y(a*)ml"] = "yaml.ansible",
         ["playbook.y(a*)ml"] = "yaml.ansible",
         [".*/tasks/.*%.y(a*)ml"] = "yaml.ansible",
         [".*/handlers/.*%.y(a*)ml"] = "yaml.ansible",
-        [".*/role(s*)/.*%.y(a*)ml"] = "yaml.ansible",
+        [".*/role(s*)/.*%.y(a*)ml"] = { "yaml.ansible", { priority = 5 } },
         [".*/playbook(s*)/.*%.y(a*)ml"] = "yaml.ansible",
         [".*/vars/.*%.y(a*)ml"] = "yaml.ansible",
         [".*/group_vars/.*%.y(a*)ml"] = "yaml.ansible",
