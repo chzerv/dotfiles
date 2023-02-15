778b623d02e3a6c1b8b5bf259f658d506d3ff029
Chris Zervakis <xrist.zerv@protonmail.com>
2023-02-16 00:28:31 +0200

    feat(nvim): Utilize 'compiler' and 'make' for Rust

--- config/nvim/after/ftplugin/rust.lua
+++ config/nvim/after/ftplugin/rust.lua
@@ -1,6 +1,5 @@
 local has_rust_tools, _ = pcall(require, "rust-tools")
 if has_rust_tools then
-
     local opts = { noremap = true, silent = true, buffer = true }
     local map = vim.keymap.set
 
@@ -10,3 +9,15 @@ if has_rust_tools then
     map("n", "<leader>rc", "<cmd>RustOpenCargo<cr>", opts)
     map("n", "<leader>rR", "<cmd>CargoReload<cr>", opts)
 end
+
+vim.cmd("compiler cargo")
+
+-- Make quickfix window show up automatically after 'make'
+vim.api.nvim_create_autocmd("QuickFixCmdPost", {
+    pattern = "[^l]*",
+    command = "cwindow",
+    nested = true,
+})
+
+vim.keymap.set("n", "<leader>mb", "<cmd>make build<cr>", { desc = "[M]ake [B]uild" })
+vim.keymap.set("n", "<leader>mc", "<cmd>make clippy -q<cr>", { desc = "[M]ake [C]lippy" })
