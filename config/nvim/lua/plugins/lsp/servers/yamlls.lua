f3fd69c9d56671e83b04327b2b8b2fd813422a63
Chris Zervakis <xrist.zerv@protonmail.com>
2023-02-26 09:49:20 +0200

    fix(nvim): No regex in yamlls schemas

--- config/nvim/lua/plugins/lsp/servers/yamlls.lua
+++ config/nvim/lua/plugins/lsp/servers/yamlls.lua
@@ -7,10 +7,10 @@ return {
                 enable = true,
             },
             schemas = {
-                kubernetes = "/(?!kustomization)*.yaml",
-                ["https://json.schemastore.org/kustomization.json"] = "kustomization.yaml",
+                kubernetes = { "k8s/*.yaml", "manifests/*.yaml" },
+                ["https://json.schemastore.org/kustomization.json"] = "kustomization.{yaml, yml}",
                 ["https://json.schemastore.org/github-workflow.json"] = "/.github/workflows/*",
-                ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "docker-compose*.yml",
+                ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "docker-compose*.{yml, yaml}",
                 ["https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json"] = ".gitlab-ci.yml",
             },
         },
