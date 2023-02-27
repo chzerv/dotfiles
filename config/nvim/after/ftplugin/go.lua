c053e6f57efce55559746cd4a07242b3ec94aac9
Chris Zervakis <xrist.zerv@protonmail.com>
2023-02-27 20:27:28 +0200

    feat(nvim): Utilize `makeprg` and `make` for Golang

--- config/nvim/after/ftplugin/go.lua
+++ config/nvim/after/ftplugin/go.lua
@@ -2,3 +2,8 @@ vim.opt_local.expandtab = false
 vim.opt_local.shiftwidth = 4
 vim.opt_local.softtabstop = 4
 vim.opt_local.tabstop = 4
+
+vim.opt_global.makeprg = "go"
+
+vim.keymap.set("n", "<leader>mr", "<cmd>make run .<cr>", { desc = "[M]ake [R]un" })
+vim.keymap.set("n", "<leader>mt", "<cmd>make test -v<cr>", { desc = "[M]ake [T]est" })
