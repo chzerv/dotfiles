1a00691fd680c81c870b52e7eecd05f52aa15d10
Chris Zervakis <xrist.zerv@protonmail.com>
2023-02-27 20:24:23 +0200

    feat(nvim): Improve `path` handling for Ansible

--- config/nvim/after/ftplugin/ansible.lua
+++ config/nvim/after/ftplugin/ansible.lua
@@ -2,5 +2,19 @@ if vim.fn.executable("ansible-doc") then
     vim.bo.keywordprg = ":sp term://ansible-doc"
 end
 
--- Search in the 'templates' and 'files' directories when using 'gf'
-vim.bo.path = vim.bo.path .. ",templates/,files/"
+-- All credits to: https://github.com/mfussenegger/nvim-ansible/
+-- Replace the "tasks" directory with "files" and "templates" and add the directories to the "path"
+-- E.g., If we are in $HOME/projects/ansible/tasks/main.yml, "path" contains:
+--  + $HOME/projects/ansible/tasks/*
+--  + $HOME/projects/ansible/files/*
+--  + $HOME/projects/ansible/templates/*
+local fname = vim.api.nvim_buf_get_name(0)
+if fname:find("tasks/") then
+    local paths = {
+        vim.bo.path,
+        vim.fs.dirname(fname:gsub("tasks/", "files/")),
+        vim.fs.dirname(fname:gsub("tasks/", "templates/")),
+        vim.fs.dirname(fname),
+    }
+    vim.bo.path = table.concat(paths, ",")
+end
